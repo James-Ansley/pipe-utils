fc03f6b963f224a55f836ad057eb772a55daf107
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2023-02-19 21:40:07 +1300

    added more value utils

--- src/pipe_utils/values.py
+++ src/pipe_utils/values.py
@@ -1,9 +1,10 @@
+import operator
 from collections.abc import Callable
-from typing import Protocol, TypeVar
+from typing import Any, Protocol, TypeVar
 
 T = TypeVar("T")
 V = TypeVar("V")
-
+R = TypeVar("R")
 
 __all__ = [
     "is_even",
@@ -11,6 +12,29 @@ __all__ = [
     "is_congruent",
     "is_non_negative",
     "is_non_positive",
+    "is_none",
+    "is_not_none",
+    "add_by",
+    "sub_by",
+    "mul_by",
+    "div_by",
+    "fdiv_by",
+    "mod_by",
+    "matmul_by",
+    "to_power",
+    "eq",
+    "ne",
+    "lt",
+    "le",
+    "gt",
+    "ge",
+    "it_is",
+    "it_is_not",
+    "bit_and",
+    "bit_or",
+    "bit_xor",
+    "rshift",
+    "lshift",
 ]
 
 
@@ -51,3 +75,118 @@ def is_non_negative(item: SupportsGE) -> bool:
 def is_non_positive(item: SupportsLE) -> bool:
     """Returns True if the given item is less than or equal to 0"""
     return item <= 0
+
+
+def is_none(item: Any) -> bool:
+    """Returns True if the given item is None"""
+    return item is None
+
+
+def is_not_none(item: Any) -> bool:
+    """Returns True if the given item is not None"""
+    return item is not None
+
+
+def add_by(n: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x + n"""
+    return lambda item: operator.add(item, n)
+
+
+def sub_by(n: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x - n"""
+    return lambda item: operator.sub(item, n)
+
+
+def mul_by(n: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x * n"""
+    return lambda item: operator.mul(item, n)
+
+
+def div_by(n: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x / n"""
+    return lambda item: operator.truediv(item, n)
+
+
+def fdiv_by(n: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x // n"""
+    return lambda item: operator.floordiv(item, n)
+
+
+def mod_by(n: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x % n"""
+    return lambda item: operator.mod(item, n)
+
+
+def matmul_by(n: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x @ n"""
+    return lambda item: operator.matmul(item, n)
+
+
+def to_power(n: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x ** n"""
+    return lambda item: operator.pow(item, n)
+
+
+def eq(other: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x == n"""
+    return lambda item: operator.eq(item, other)
+
+
+def ne(other: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x != n"""
+    return lambda item: operator.ne(item, other)
+
+
+def lt(other: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x < n"""
+    return lambda item: operator.lt(item, other)
+
+
+def le(other: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x <= n"""
+    return lambda item: operator.le(item, other)
+
+
+def gt(other: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x > n"""
+    return lambda item: operator.gt(item, other)
+
+
+def ge(other: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x >= n"""
+    return lambda item: operator.ge(item, other)
+
+
+def it_is(obj: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x is n"""
+    return lambda item: operator.is_(item, obj)
+
+
+def it_is_not(obj: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x is not n"""
+    return lambda item: operator.is_not(item, obj)
+
+
+def bit_and(value: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x & value"""
+    return lambda item: operator.and_(item, value)
+
+
+def bit_or(value: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x | value"""
+    return lambda item: operator.or_(item, value)
+
+
+def bit_xor(value: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x ^ value"""
+    return lambda item: operator.xor(item, value)
+
+
+def rshift(n: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x >> n"""
+    return lambda item: operator.rshift(item, n)
+
+
+def lshift(n: Any) -> Callable:
+    """Returns a callable that takes a parameter x and returns x << n"""
+    return lambda item: operator.lshift(item, n)
--- tests/test_values.py
+++ tests/test_values.py
@@ -56,3 +56,170 @@ def test_is_non_positive():
 
     assert is_non_positive(Fraction(-1, 1))
     assert not is_non_positive(Fraction(1, 1))
+
+
+def test_is_none():
+    assert is_none(None)
+    assert not is_none([])
+
+
+def test_is_not_none():
+    assert not is_not_none(None)
+    assert is_not_none([])
+
+
+def test_add_by():
+    assert add_by(1)(1) == 2
+    assert add_by(1)(-1) == 0
+    assert add_by(-1)(1) == 0
+    assert add_by(Fraction(1, 2))(Fraction(1, 2)) == Fraction(1, 1)
+
+
+def test_sub_by():
+    assert sub_by(1)(1) == 0
+    assert sub_by(1)(-1) == -2
+    assert sub_by(-1)(1) == 2
+    assert sub_by(Fraction(1, 2))(Fraction(1, 2)) == Fraction(0, 1)
+
+
+def test_mul_by():
+    assert mul_by(1)(1) == 1
+    assert mul_by(1)(-1) == -1
+    assert mul_by(-1)(1) == -1
+    assert mul_by(Fraction(1, 2))(Fraction(1, 2)) == Fraction(1, 4)
+
+    assert mul_by(2)("a") == "aa"
+
+
+def test_div_by():
+    assert div_by(1)(1) == 1.0
+    assert div_by(1)(-1) == -1.0
+    assert div_by(-1)(1) == -1.0
+    assert div_by(2)(10) == 5.0
+    assert div_by(Fraction(1, 2))(Fraction(1, 2)) == Fraction(1, 1)
+
+
+def test_fdiv_by():
+    assert fdiv_by(1)(1) == 1
+    assert fdiv_by(1)(-1) == -1
+    assert fdiv_by(-1)(1) == -1
+    assert fdiv_by(2)(10) == 5
+    assert fdiv_by(Fraction(1, 2))(Fraction(1, 2)) == Fraction(1, 1)
+    assert fdiv_by(Fraction(2, 1))(Fraction(1, 1)) == Fraction(0, 1)
+
+
+def test_mod_by():
+    assert mod_by(2)(0) == 0
+    assert mod_by(2)(2) == 0
+    assert mod_by(2)(1) == 1
+    assert mod_by(2)(3) == 1
+    assert mod_by(5)(6) == 1
+
+
+def test_to_power():
+    assert to_power(2)(2) == 4
+    assert to_power(0)(2) == 1
+    assert to_power(1)(0) == 0
+    assert to_power(1)(1) == 1
+    assert to_power(0.5)(4) == 2.0
+
+
+def test_eq():
+    assert eq(5)(5)
+    assert not eq(5)(4)
+    assert eq([1, 2])([1, 2])
+    assert not eq([1, 2])([1, 2, 3])
+
+
+def test_ne():
+    assert ne(5)(4)
+    assert ne([1, 2])([1, 2, 3])
+    assert not ne(5)(5)
+    assert not ne([1, 2])([1, 2])
+
+
+def test_lt():
+    assert lt(6)(5)
+    assert not lt(3)(4)
+    assert not lt(3)(3)
+    assert lt("b")("a")
+    assert not lt("a")("b")
+
+
+def test_gt():
+    assert gt(5)(6)
+    assert not gt(5)(5)
+    assert not gt(5)(4)
+    assert gt("a")("b")
+    assert not gt("b")("a")
+
+
+def test_le():
+    assert le(6)(5)
+    assert not le(3)(4)
+    assert le(3)(3)
+    assert le("b")("a")
+    assert not le("a")("b")
+    assert le("a")("a")
+
+
+def test_ge():
+    assert ge(5)(6)
+    assert ge(5)(5)
+    assert not ge(5)(4)
+    assert ge("a")("b")
+    assert not ge("b")("a")
+    assert ge("a")("a")
+
+
+def test_it_is():
+    a, b = object(), object()
+    assert it_is(a)(a)
+    assert it_is(None)(None)
+    assert not it_is(a)(b)
+    assert not it_is(None)(b)
+    assert not it_is(a)(None)
+    assert not it_is([1, 2])([1, 2])
+
+
+def test_it_is_not():
+    a, b = object(), object()
+    assert not it_is_not(a)(a)
+    assert not it_is_not(None)(None)
+    assert it_is_not(a)(b)
+    assert it_is_not(None)(b)
+    assert it_is_not(a)(None)
+    assert it_is_not([1, 2])([1, 2])
+
+
+def test_bit_and():
+    assert bit_and(0b0101)(0b1010) == 0b0000
+    assert bit_and(0b1010)(0b1010) == 0b1010
+    assert bit_and(0b00)(0b11) == 0b00
+    assert bit_and(0b11)(0b11) == 0b11
+
+
+def test_bit_or():
+    assert bit_or(0b0101)(0b1010) == 0b1111
+    assert bit_or(0b1010)(0b1010) == 0b1010
+    assert bit_or(0b00)(0b11) == 0b11
+    assert bit_or(0b11)(0b11) == 0b11
+
+
+def test_bit_xor():
+    assert bit_xor(0b0101)(0b1010) == 0b1111
+    assert bit_xor(0b1010)(0b1010) == 0b0000
+    assert bit_xor(0b00)(0b11) == 0b11
+    assert bit_xor(0b11)(0b11) == 0b00
+
+
+def test_rshift():
+    assert rshift(1)(2) == 1
+    assert rshift(1)(4) == 2
+    assert rshift(2)(8) == 2
+
+
+def test_lshift():
+    assert lshift(1)(2) == 4
+    assert lshift(1)(4) == 8
+    assert lshift(2)(8) == 32
